c7b3b15099afe0076a03f6214efaebaaa8d4d596
AlfaRiza <malfariza45@gmail.com>
2020-07-06 08:08:16 +0700

    component creater

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Logos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Logos from "./components/Logos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // HelloWorld,
+    Logos
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Logos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="logos-wrapper">
+    <h2>Find the Best Logos</h2>
+    <div class="item-logos">
+      <img src alt />
+    </div>
+    <div class="item-logos">
+      <img src alt />
+    </div>
+    <div class="item-logos">
+      <img src alt />
+    </div>
+    <div class="item-logos">
+      <img src alt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logos"
+};
+</script>
+
+<style scoped>
+.logos-wrapper {
+  width: 80%;
+  background: green;
+  margin: auto;
+  overflow: hidden;
+}
+
+.item-logos {
+  float: left;
+  width: 150px;
+  height: 150px;
+  background: blue;
+  margin: 0 10px;
+  /* margin: auto; */
+}
+</style>
